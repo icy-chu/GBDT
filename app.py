61dd9b9615c74e2a90fa838381f73ec2e84c9598
icy-chu <yilichu0618@163.com>
2025-08-22 01:05:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def index():
     return render_template('index.html')
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))  # 默认端口为 5001
-    app.run(host='127.0.0.1', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
